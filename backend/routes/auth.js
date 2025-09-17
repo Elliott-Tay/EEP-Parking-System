209645c8d6fd5dcf23ccdfc55963d47f2c75369d
Elliott Tay <elliott.tay@gtech.com.sg>
2025-09-17 13:38:59 +0800

    remove JWT error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,6 @@ const { sql, config } = require("../database/db");
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
-if (!JWT_SECRET) throw new Error("JWT_SECRET not set!");
 
 // ======== Initialize SQL Pool ========
 let pool;
